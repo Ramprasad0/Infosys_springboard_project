57c0ad69a6170f718e567c4cacbc26f7a8e7fc75
ramprasad <ramprasadsunkara008@gmail.com>
2024-12-15 19:35:17 +0530

    added module 5 payment

--- module2/src/main/java/com/example/module2/Module2Application.java
+++ module2/src/main/java/com/example/module2/Module2Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient  // Enables Module 2 to register with Eureka// Enables the module to register with Eureka
+@EnableFeignClients
 public class Module2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Module2Application.class, args);
